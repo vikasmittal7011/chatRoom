89011d4657f5e77762b13e1e49bf3d0de7db39ee
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-21 15:11:01 +0530

    Pause google verification

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+// import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import Chats from "./Chats";
 
@@ -7,27 +7,36 @@ function ChatRoom() {
   const [userName, setUserName] = useState(
     localStorage.getItem("userName") || ""
   );
-  const provider = new GoogleAuthProvider();
+  // const provider = new GoogleAuthProvider();
 
-  const auth = getAuth();
-  const googleLogin = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        setUserName(user.displayName);
-        localStorage.setItem("userName", user.displayName);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const auth = getAuth();
+  // const googleLogin = () => {
+  //   signInWithPopup(auth, provider)
+  //     .then((result) => {
+  //       const user = result.user;
+  //       setUserName(user.displayName);
+  //       localStorage.setItem("userName", user.displayName);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
   return (
     <div>
       {!userName && (
-        <button className="btn btn-outline-danger w-100" onClick={googleLogin}>
+        <input
+          type="text"
+          onBlur={(e) => {
+            setUserName(e.target.value);
+            localStorage.setItem("userName", e.target.value);
+          }}
+          className="form-control"
+          placeholder="Enter your name & click outside the box to continue..."
+        />
+        /* <button className="btn btn-outline-danger w-100" onClick={googleLogin}>
           Signin With Google
-        </button>
+        </button> */
       )}
       {userName && <Chats name={userName} setUser={setUserName} />}
     </div>
